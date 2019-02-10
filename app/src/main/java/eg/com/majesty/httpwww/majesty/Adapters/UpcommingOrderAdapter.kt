c44f0efd5974cf3e391b1c084dce74a0f121b165
Taha <tahaelhosseny@gmail.com>
2019-02-10 23:50:12 +0200

    done 2 feb 2018

--- app/src/main/java/eg/com/majesty/httpwww/majesty/Adapters/UpcommingOrderAdapter.kt
+++ app/src/main/java/eg/com/majesty/httpwww/majesty/Adapters/UpcommingOrderAdapter.kt
@@ -27,44 +27,86 @@ class UpcommingOrderAdapter (var activity : Activity , var upcommingOrdersModels
     override fun onBindViewHolder(holder: MyViewHolder, position: Int)
     {
 
-        holder.orderId.setTypeface(Utils.Exo2SemiBold(activity))
-        holder.ItemName.setTypeface(Utils.Exo2SemiBold(activity))
-        holder.estimatedArrival.setTypeface(Utils.Exo2SemiBold(activity))
-        holder.time.setTypeface(Utils.Exo2SemiBold(activity))
-        holder.totalPrice.setTypeface(Utils.Exo2SemiBold(activity))
-        holder.itemSize.setTypeface(Utils.setExo2Regular(activity))
-        holder.tot.setTypeface(Utils.setExo2Regular(activity))
-
-
-        Glide.with(activity).load(upcommingOrdersModels.get(position).OrderImageUrl).thumbnail(.2f).into(holder.FoodMenuImageUrl)
-        holder.orderId.text = upcommingOrdersModels.get(position).OrderNo.toString()
-        holder.totalPrice.text = upcommingOrdersModels.get(position).Total.toString()
-
-
-
-
-        if( upcommingOrdersModels.get(position).COnfirmDateTimeTS.equals(""))
-            holder.progress.setCurrentProgress(100)
-        else
+        if(upcommingOrdersModels.get(position).StatusDesc.equals("Pending"))
         {
-            var current = ( upcommingOrdersModels.get(position).COnfirmDateTimeTS.toLong() + upcommingOrdersModels.get(position).ExpectedDeliveryMinues*60) -(System.currentTimeMillis()/1000)
 
-            if(current.toInt()<0)
-            {
-                current = (0).toFloat()
-                holder.time.text = "00:00"
-                holder.progress.setCurrentProgress(current.toInt())
+            holder.first.visibility = View.GONE
+            holder.second.visibility = View.VISIBLE
 
-            }
+            holder.sttext.text = "Pending"
+            Glide.with(activity).load(R.drawable.ic_pen).thumbnail(.2f).into(holder.statIcon)
+            holder.statIcon
+
+
+
+            holder.orderId2.setTypeface(Utils.Exo2SemiBold(activity))
+            holder.ItemName2.setTypeface(Utils.Exo2SemiBold(activity))
+            holder.date2.setTypeface(Utils.Exo2SemiBold(activity))
+            holder.sttext.setTypeface(Utils.Exo2SemiBold(activity))
+
+
+
+
+            Glide.with(activity).load(upcommingOrdersModels.get(position).OrderImageUrl).thumbnail(.2f).into(holder.FoodMenuImageUrl2)
+            holder.orderId2.text = upcommingOrdersModels.get(position).OrderNo.toString()
+            holder.totalPrice2.text = upcommingOrdersModels.get(position).Total.toString()
+
+            holder.date2.text = Utils.getDate(upcommingOrdersModels.get(position).OrderDateTS.toLong())
+
+
+
+
+        }else
+        {
+            holder.orderId.setTypeface(Utils.Exo2SemiBold(activity))
+            holder.ItemName.setTypeface(Utils.Exo2SemiBold(activity))
+            holder.estimatedArrival.setTypeface(Utils.Exo2SemiBold(activity))
+            holder.time.setTypeface(Utils.Exo2SemiBold(activity))
+            holder.totalPrice.setTypeface(Utils.Exo2SemiBold(activity))
+            holder.itemSize.setTypeface(Utils.setExo2Regular(activity))
+            holder.tot.setTypeface(Utils.setExo2Regular(activity))
+
+
+            Glide.with(activity).load(upcommingOrdersModels.get(position).OrderImageUrl).thumbnail(.2f).into(holder.FoodMenuImageUrl)
+            holder.orderId.text = upcommingOrdersModels.get(position).OrderNo.toString()
+            holder.totalPrice.text = upcommingOrdersModels.get(position).Total.toString()
+
+
+
+
+            if( upcommingOrdersModels.get(position).COnfirmDateTimeTS.equals(""))
+                holder.progress.setCurrentProgress(100)
             else
             {
+                var current = ( upcommingOrdersModels.get(position).COnfirmDateTimeTS.toLong() + upcommingOrdersModels.get(position).ExpectedDeliveryMinues*60) -(System.currentTimeMillis()/1000)
 
-                holder.time.text
-                current = (current * 100) /  upcommingOrdersModels.get(position).ExpectedDeliveryMinues*60
-                holder.progress.setCurrentProgress(current.toInt())
+                if(current.toInt()<0)
+                {
+                    current = (0).toFloat()
+                    holder.time.text = "00:00"
+                    holder.progress.setCurrentProgress(current.toInt())
+
+                }
+                else
+                {
+
+                    holder.time.text
+                    current = (current * 100) /  upcommingOrdersModels.get(position).ExpectedDeliveryMinues*60
+                    holder.progress.setCurrentProgress(current.toInt())
+                }
+
             }
 
         }
+
+
+
+
+
+
+
+
+
 
 
 
@@ -82,6 +124,20 @@ class UpcommingOrderAdapter (var activity : Activity , var upcommingOrdersModels
         val tot = itemView.tot
         val totalPrice = itemView.totalPrice
         val progress = itemView.progress
+
+
+
+
+        val FoodMenuImageUrl2 = itemView.FoodMenuImageUrl2
+        val orderId2 = itemView.orderId2
+        val ItemName2 = itemView.ItemName2
+        val sttext = itemView.sttext
+        val statIcon = itemView.statIcon
+        val date2 = itemView.date2
+        val totalPrice2 = itemView.totalPrice2
+        val first = itemView.first
+        val second = itemView.second
+
 
 
     }
